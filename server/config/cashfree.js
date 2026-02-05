7f39dbb508323fef55d950e313baff4448829bc1
publicsworks <professionalwork04@gmail.com>
2026-02-05 11:56:40 +0530

    Fix Cashfree Environment configuration

--- server/config/cashfree.js
+++ server/config/cashfree.js
@@ -3,7 +3,7 @@ const { Cashfree } = require('cashfree-pg');
 Cashfree.XClientId = process.env.CASHFREE_APP_ID;
 Cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
 Cashfree.XEnvironment = process.env.CASHFREE_ENV === 'PRODUCTION'
-    ? Cashfree.Environment.PRODUCTION
-    : Cashfree.Environment.SANDBOX;
+    ? "PRODUCTION"
+    : "SANDBOX";
 
 module.exports = Cashfree;
